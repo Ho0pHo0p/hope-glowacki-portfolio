be311e8872f29f557b94c8837a572c4c89a9ee4e
Hope G <119355967+Ho0pHo0p@users.noreply.github.com>
2023-08-08 21:05:02 -0400

    About me content

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -10,7 +10,7 @@ export default function AboutMe(){
       <img src={image}></img>
       <div>
         <h4>About Me</h4>
-        <p>Hi, I'm Hope Glowacki! A film student turned web developer. </p>
+        <p>Hello! I'm Hope Glowacki, a detail-driven Front-End Web Developer with a passion for creating immersive digital experiences. Armed with React, JavaScript, HTML5, and CSS skills, I blend creativity with technology to design user-friendly solutions. From crafting apps that simplify exploration to enhancing screenwriters' planning, I thrive on transforming ideas into engaging digital realities. With a background in film and media studies, I bring storytelling flair to coding and design. </p>
         <span>
           <a href="https://www.linkedin.com/in/hope-glowacki" target="_blank"><FontAwesomeIcon icon={faLinkedin}/></a>
           <a href="https://github.com/Ho0pHo0p" target="_blank"><FontAwesomeIcon icon={faGithub}/></a>
